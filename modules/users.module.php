0fae4eccc49feaa2bd8a3b385c9225a7a72f0499
SkaN2412 <andreykamozin@gmail.com>
2012-08-08 17:16:22 +0400

    Написал метод для авторизации пользователей

--- modules/users.module.php
+++ modules/users.module.php
@@ -9,7 +9,7 @@ class User
     protected static $login;
     protected static $email;
     protected static $group;
-    protected static $blocked_until;
+    protected static $blocked_until = NULL;
     /*
      * function user_register() adds user to DB and euthorizes him
      */
@@ -60,7 +60,40 @@ class User
     */
     public static function authorize($login, $password)
     {
-
+        // Connect to DB
+        $DBH = db_connect();
+        
+        // Generate hash of password
+        $crypt = new Bcrypt(15);
+        $hash = $crypt->hash($password);
+        
+        // Get data from DB
+        $stmt = $DBH->prepare("SELECT * FROM `users` WHERE `login` = :login");
+        $stmt->execute( array( 'login' => $login ) );
+        // If nothing is returned, throw exception
+        if ( $stmt->rowCount() < 1 )
+        {
+            throw new inviException(4, "Login is not registered");
+        }
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $userData = $stmt->fetch();
+        
+        // Check password correctness
+        if ( ! $crypt->verify( $password, $userData['password'] ) )
+        {
+            throw new inviException(5, "Incorrect password");
+        }
+        
+        // Fill class properties with data selected
+        self::$authed = TRUE;
+        self::$login = $userData['login'];
+        self::$email = $userData['email'];
+        self::$group = $userData['group'];
+        if ( $userData['blocked_until'] != NULL )
+        {
+            self::$blocked_until = $userData['blocked_until'];
+        }
+        return TRUE;
     }
     /*
      * Method get() returns data of user. If you call it without parameters, it will return all user's data in array. Or you can call it with parameter "what to return". You can get login, email or group. If you call this method, while user is not authorized, it will throw exception
