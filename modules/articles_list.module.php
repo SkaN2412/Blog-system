cb295529c4d98d349cb00215419174442c8d014c
SkaN2412 <andreykamozin@gmail.com>
2012-08-09 12:30:28 +0400

    Небольшие изменения в функции загрузки списка статей. Написана функция загрузки категорий определенного родителя

--- modules/articles_list.module.php
+++ modules/articles_list.module.php
@@ -2,58 +2,32 @@
 /*
  * Function 'articles_list' returns multi-dimensional array with 10 articles.
  * Parameter $pagenum is nessesary, it says for which page function should load articles. Function loads from the end
- * Parameter $filter is optional. It says which category to load. If category given does not exist, will be returned FALSE.
  */
-function articles_list($page, $filter = "all")
+function articles_list($page)
 {
     //Connect to database. $DBH is DataBase Handler. About PDO you can know here: http://habrahabr.ru/post/137664/
     $DBH = db_connect();
-    
-    //If $filter != "all", check category given for existing
-    if ($filter != "all")
-    {
-        $stmt = $DBH->prepare("SELECT `id` FROM `categories` WHERE `id` = :category");
-        $stmt = execute(array( 'category' => $filter ));
-        //If MySQL returned empty result, return false
-        if ($stmt->rowCount() == 0)
-        {
-            return FALSE;
-        }
-        $stmt = NULL;
-    }
     
     /*
      * Load articles for page given
      */
     //Watch config - how much articles per page. $EPP is Entries Per Page
     $EPP = config_get("blog->entriesPerPage");
+    
     //Count all the entries in database and get {$pagenum}-last entries.
     $query = "SELECT COUNT(*) FROM `articles` WHERE `confirmed` = 1";
-    $data = array();
-    if ($filter != "all")
-    {
-        $query .= " AND `category` = :category";
-        $data['category'] = $filter;
-    }
-    $stmt = $DBH->prepare($query);
     $stmt->execute($data);
     $stmt->setFetchMode(PDO::FETCH_NUM);
     $entriesNum = $stmt->fetch();
     $entriesNum = $entriesNum[0];
     $stmt = NULL;
+    
     //Articles are loading from the end. For example: there are 33 articles. On 1st page will be articles from 33 to 24, on 2nd page - 23-14 etc.
     $startEntry = ( $entriesNum - ( ( $EPP * $page ) - 1) );
+    
     //Selecting entries
-    $query = "SELECT `id`, `author`, `name`, `preview`, `category` FROM `articles` WHERE `confirmed` = 1 ";
-    $data = array();
-    if ($filter != "all")
-    {
-        $query .= "AND `category` = :category ";
-        $data['category'] = $filter;
-    }
-    $query .= "LIMIT {$startEntry}, {$EPP}";
-    $stmt = $DBH->prepare($query);
-    $stmt->execute($data);
+    $stmt = $DBH->prepare("SELECT `id`, `author`, `name`, `preview`, `category` FROM `articles` WHERE `confirmed` = 1 LIMIT {$startEntry}, {$EPP}");
+    $stmt->execute();
     $stmt->setFetchMode(PDO::FETCH_ASSOC);
     $clean = array();
     $judged = array();
--- modules/categories.module.php
+++ modules/categories.module.php
@@ -4,10 +4,42 @@
  */
 /*
  * categories_get() function return array with categories of parent given
- * parameter $parent says, which parent to load. If 0 given, then load 1st level categories
+ * parameter $parent says, which parent to load. By default function loads 1st level categories
+ * Function returns FALSE in case of unexisting parent
+ * Function returns NULL in case of there's no category's children in DB
  */
-function categories_get($parent)
+function categories_get( $parent = 0 )
 {
+    // Connect to DB
+    $DBH = db_connect();
     
+    // If parent isn't 0, check it for existing
+    if ( $parent != 0 )
+    {
+        $stmt = $DBH->prepare("SELECT `id` FROM `categories` WHERE `id` = :id");
+        $stmt->execute( array( 'id' => $parent ) );
+        if ( $stmt->rowCount() < 1 )
+        {
+            return FALSE;
+        }
+    }
+    
+    // Select categories IDs and names
+    $stmt = $DBH->prepare("SELECT * FROM `categories` WHERE `parent` = :parent");
+    $stmt->execute( array( 'parent' => $parent ) );
+    // If no entries returned, return NULL
+    if ( $stmt->rowCount() < 1 )
+    {
+        return NULL;
+    }
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $children = array();
+    while ( $child = $stmt->fetch() )
+    {
+        $children[] = $child;
+    }
+    
+    // Return children
+    return $categories;
 }
 ?>
